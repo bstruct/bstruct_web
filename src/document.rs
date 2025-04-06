7cef7e2515b45b383130cfbb1bf4f55b058fb090
Damiao Castro <damiaocastro@pvh.com>
2025-04-06 16:26:48 +0200

    set_custom_event_listener

--- src/document.rs
+++ src/document.rs
@@ -90,6 +90,28 @@ impl HtmlNode {
 
         Ok(self.clone())
     }
+
+    pub fn set_custom_event_listener<T>(
+        &self,
+        type_: &str,
+        f: T,
+    ) -> BaseResult<HtmlNode>
+    where
+        T: Fn(&web_sys::CustomEvent),
+        T: 'static,
+    {
+        let on_event_type_closure: Closure<dyn Fn(&web_sys::CustomEvent)> =
+            Closure::wrap(Box::new(f) as Box<dyn Fn(&web_sys::CustomEvent)>);
+
+        self.to_element_node()?
+            .add_event_listener_with_callback(type_, on_event_type_closure.as_ref().unchecked_ref())
+            .to_base_result()?;
+
+        on_event_type_closure.forget();
+
+        Ok(self.clone())
+    }
+
 }
 
 pub trait ResultJs<T: std::clone::Clone> {
