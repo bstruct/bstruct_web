106a27111ce3a91832158712ab8f0f8e04fdadbc
DamiaoCastro <damiao.c@hotmail.com>
2025-12-27 17:48:13 +0100

    test1

--- src/custom_element.rs
+++ src/custom_element.rs
@@ -104,7 +104,7 @@ pub fn define_custom_element(
     // let class = format!("(class BstructCustomElement extends HTMLElement {{ constructor(){{super();}} connectedCallback(){{ {}(this); }} }})", custom_element_definition.connected_callback_function_name);
     let class = "(class BstructCustomElement extends HTMLElement{
     constructor(){super();}
-    connectedCallback(){ debugger; this.dispatch_event(new Event('connected'));}
+    connectedCallback(){ debugger;}
     disconnectedCallback(){}`
     connectedMoveCallback(){}
     adoptedCallback(){}
